fa176cda8a8ee2c520c5e787c6e21fc0ab84ebe6
Caio Albuquerque <caio.albuquerque@infobase.com.br>
2022-07-01 18:36:03 -0300

    Create migrations

---
+++ app/Http/Controllers/CartProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\cart_product;
+use Illuminate\Http\Request;
+
+class CartProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\cart_product  $cart_product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(cart_product $cart_product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\cart_product  $cart_product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(cart_product $cart_product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\cart_product  $cart_product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, cart_product $cart_product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\cart_product  $cart_product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(cart_product $cart_product)
+    {
+        //
+    }
+}
